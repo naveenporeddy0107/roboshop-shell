d31ca2748355e3c1f846bad5ca17de06a24a3c5e
nreddy4 <naveenkumar9707@gmail.com>
2023-04-30 16:17:42 -0500

    rabbitmq.sh changes

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -2,7 +2,8 @@ curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.
 yum install erlang -y
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
 yum install rabbitmq-server -y
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+systemctl enable rabbitmq-server
+systemctl start rabbitmq-server
