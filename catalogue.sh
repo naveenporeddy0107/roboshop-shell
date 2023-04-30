e279d6fd1c67488c1441a4612c6f51ce8cea06a5
nreddy4 <naveenkumar9707@gmail.com>
2023-04-30 14:56:32 -0500

    adding echo stmts

--- catalogue.sh
+++ catalogue.sh
@@ -1,17 +1,45 @@
+echo -e "\e[32m>>>>>>>>>>>>> Configuring node js repos <<<<<<<<<<<<<<<<< \e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[32m>>>>>>>>>>> installing node js >>>>>>>\e[0m"
 yum install nodejs -y
+echo -e "\e[32m>>>>>>>>>>> adding a user>>>>>>>\e[0m"
+
 useradd roboshop
+echo -e "\e[32m>>>>>>>>>>> making a directory>>>>>>>\e[0m"
+
 mkdir /app
+echo -e "\e[32m>>>>>>>>>>> downloding catalogue source code>>>>>>>\e[0m"
+
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+echo -e "\e[32m>>>>>>>>>>> switching to app folder>>>>>>>\e[0m"
+
 cd /app
+echo -e "\e[32m>>>>>>>>>>> unzipping catalogue source code>>>>>>>\e[0m"
+
 unzip /tmp/catalogue.zip
 
+echo -e "\e[32m>>>>>>>>>>> Installing node tool>>>>>>>\e[0m"
+
 npm install
+echo -e "\e[32m>>>>>>>>>>> copying catalogue service file>>>>>>>\e[0m"
 
 cp catalogue.service /etc/systemd/system/catalogue.service
+echo -e "\e[32m>>>>>>>>>>> system ctl >>>>>>>\e[0m"
+
 systemctl daemon-reload
+echo -e "\e[32m>>>>>>>>>>> enabling catalogue  >>>>>>>\e[0m"
+
 systemctl enable catalogue
-systemctl start catalogue
+echo -e "\e[32m>>>>>>>>>>> restarting catalogue service >>>>>>>\e[0m"
+
+systemctl restart catalogue
+echo -e "\e[32m>>>>>>>>>>> configuring mongodb repos>>>>>>>\e[0m"
+
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[32m>>>>>>>>>>> installing mongodb >>>>>>>\e[0m"
+
 yum install mongodb-org-shell -y
+echo -e "\e[32m>>>>>>>>>>> installing mongodb schema >>>>>>>\e[0m"
+
 mongo --host mongo.naveendevops.tech </app/schema/catalogue.js
