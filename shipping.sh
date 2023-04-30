ccc44b57ad12eb72fe03fbbcb4b0d1bb27b6818d
nreddy4 <naveenkumar9707@gmail.com>
2023-04-30 16:06:50 -0500

    shipping sh changes

--- shipping.sh
+++ shipping.sh
@@ -1,15 +1,17 @@
 yum install maven -y
 useradd roboshop
-cp shipping.service /etc/systemd/system/shipping.service
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
+rm -rf /app
 mkdir /app
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
 cd /app
 unzip /tmp/shipping.zip
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
-systemctl daemon-reload
-systemctl enable shipping
-systemctl start shipping
+
 yum install mysql -y
 mysql -h sql.naveendevops.tech -uroot -pRoboShop@1 < /app/schema/shipping.sql
+
+systemctl daemon-reload
+systemctl enable shipping
 systemctl restart shipping
